9cd92308892f29762ddc87f380f4535f86a4626f
Adam Argyle <atom@argyleink.com>
2015-01-05 13:47:00 -0800

    squared out cat demo photos

--- app/js/example.js
+++ app/js/example.js
@@ -7,12 +7,12 @@ nGon.init('demo', {
       '<div class="poly"><h1>Second:Middle</h1></div>',
       '<div class="poly"><h1>Bottom</h1></div>'
     ],
-    '<img src="https://placekitten.com/g/500/500">',
+    '<img src="https://placekitten.com/g/600/600">',
     '<h1>Fourth</h1>',
     [
       '<div class="poly"><img src="https://placekitten.com/g/500/500"></div>',
-      '<div class="poly"><img src="https://placekitten.com/g/800/500"></div>',
-      '<div class="poly"><img src="https://placekitten.com/g/700/500"></div>'
+      '<div class="poly"><img src="https://placekitten.com/g/800/800"></div>',
+      '<div class="poly"><img src="https://placekitten.com/g/700/700"></div>'
     ],
     '<h1>Sixth</h1>'
   ]
